2c7c8f6e63a08ffafa7dfcee604cf8e9b65cc5c8
Myozitol <77352149+Myozitol@users.noreply.github.com>
2024-01-12 23:50:51 +0300

    Add files via upload

---
+++ lesson-79/main.js
@@ -0,0 +1,11 @@
+let age = 29
+
+if (age >= 18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вам еще нет 18')
+}
+
+let ageTer = 15
+
+ageTer >= 18 ? console.log('Вы совершеннолетний') :  console.log('Вам еще нет 18')
