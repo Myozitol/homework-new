a70e79571167273306127f64cb5392507470aede
Myozitol <77352149+Myozitol@users.noreply.github.com>
2024-01-12 23:44:11 +0300

    Add files via upload

---
+++ lesson-76/main.js
@@ -0,0 +1,11 @@
+let firstName = 'John'
+let lastName = 'Snow'
+let age = '22'
+let familyName = 'Stark'
+let familyMotto = 'Winter Is Coming'
+
+console.log('firstName '+ firstName) 
+console.log('lastName '+ lastName) 
+console.log('age '+ age) 
+console.log('familyName '+ familyName) 
+console.log('familyMotto '+ familyMotto) 
