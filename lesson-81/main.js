9d1251ebc4ad4e315b2898d1e9a47d97767a1ac0
Myozitol <77352149+Myozitol@users.noreply.github.com>
2024-01-15 20:25:50 +0300

    Add files via upload

---
+++ lesson-81/main.js
@@ -0,0 +1,18 @@
+const numbers = [1, 54, 14, 78, 15, 64, 35, 75, 657, 789]
+
+for (let i = 0; i < numbers.length; i++ ) {
+    if (numbers[i] % 2 == 0) {
+        console.log(numbers[i])
+    }   
+}
+
+console.log("----------///----------")
+
+const colors = ["Красный", "Оранжевый", "Жёлтый", "Зелёный", "Голубой", "Синий", "Фиолетовый"]
+
+for (let i = colors.length; i != 0;) {
+    i = i - 1
+    console.log(colors[i])
+}
+
+
