4725554698d323953e8a315d26a7e347193f1df5
Myozitol <77352149+Myozitol@users.noreply.github.com>
2024-01-15 19:54:09 +0300

    Add files via upload

---
+++ lesson-80/main.js
@@ -0,0 +1,29 @@
+for (let i = 2; i <= 12; i++) {
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
+
+console.log("-----------////-----------")
+
+for (let i = 10; i > 0; i--) {
+    console.log(i)
+}
+
+console.log("-----------////-----------")
+
+i = 0;
+while (i < 13) {
+    i++
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
+
+console.log("-----------////-----------")
+
+i = 10;
+while (i > 0) {
+    console.log(i)
+    i--
+}
